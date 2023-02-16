9b90121ee6e6afba68fc3763dbf1b38dacf492db
kozMikhailS1286 <koz.mikhail.s@yandex.ru>
2023-02-16 17:54:18 +0300

    Vtornik, week 3/3

---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+
+export default {
+    title: "react.memo.demo",
+}
+
+const NewMessagesCounter = (props: {count: number}) => {
+    return <div> {props.count} </div>
+}
+
+const UsersSecret = (props: {users: Array<string>}) => {
+    console.log("USERS") // Не будет срабатывать, т.к. пропсы не изменились и используя React.memo
+    // она не перерисовывается. Т.к. данные именно компоненты Users не поменялись.
+    return <div>{
+        props.users.map((u, i) => <div key={i}> {u} </div>)
+    }</div>
+}
+
+const Users = React.memo(UsersSecret);
+
+
+export const Example1 = () => {
+    console.log("Example1")
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(["Dimych", "Valera", "Artem"])
+    const addUser = () => {
+        const newUsers = [...users, 'Sveta ' + new Date().getTime()]
+        setUsers(newUsers)
+    }
+
+    return <>
+        <button onClick={()=>{setCounter(counter+1)}}> + </button>
+        <button onClick={addUser}> add user </button>
+        <NewMessagesCounter count={counter}/>
+        <Users users={users}/>
+        {/*Использует Users, а сам Users использует UserSecret*/}
+    </>
+}
