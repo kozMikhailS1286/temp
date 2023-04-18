b7c5f6048491ebf7f30d3f1c74a768a35dfb7ec4
kozMikhailS1286 <koz.mikhail.s@yandex.ru>
2023-04-18 15:39:00 +0300

    Wednesday, week 2 / 1

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,21 @@
+import React, {useMemo, useState} from "react";
+
+export default {
+    title: "useState demo",
+}
+
+function generateData () {
+    console.log("generateData")
+    return 1;
+}
+export const Example1 = () => {
+    console.log("Example1")
+//    const initValue = useMemo(generateData,[])
+    const [counter, setCounter] = useState(generateData)
+    const changer = (state: number) => state + 1
+
+    return <>
+        <button onClick={()=>{setCounter(state => state + 1)}}> + </button>
+        {counter}
+    </>
+}
